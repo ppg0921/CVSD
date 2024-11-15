74f0b88b0a02884ded6dd9e8a41f3b97990b710a
betty_IC_Lab <bettycheng0921@gmail.com>
2024-11-15 11:10:45 +0800

    [HW4] 3.8E9 sad

--- 1131_hw4/01_RTL/IOTDF.v
+++ 1131_hw4/01_RTL/IOTDF.v
@@ -25,8 +25,9 @@ output [127:0] iot_out;
   localparam S_OUTPUT = 4'd5;   // output
 
 
-  reg [130:0] data, data_nxt; // Dividend and remainder, biggest, smallest, plaintext
-  reg [130:0] divisor, divisor_nxt; // Divisor, second biggest, second smallest, key
+  reg [127:0] data, data_nxt; // Dividend and remainder, biggest, smallest, plaintext
+  reg [64:0] minmax_upper, min_max_upper_nxt;
+  reg [127:0] divisor, divisor_nxt; // Divisor, second biggest, second smallest, key
   reg [127:0] iot_data_out, iot_data_out_nxt; // output data
   reg [127:0] loaded_data, loaded_data_nxt; // loaded data 
   reg [3:0] state, state_nxt;
@@ -34,34 +35,34 @@ output [127:0] iot_out;
   reg [3:0] cnt_load, cnt_load_nxt;
   wire crypt_done, crc_done, minmax_done;
   wire [63:0] crypt_plaintext_out, crypt_key_out;
-  wire [47:0] crypt_text_intermediate_out;
   wire [6:0] crc_remainder_out;
   wire [127:0] minmax_reg1_out, minmax_reg2_out;
   reg total_o_valid;
   reg o_busy_reg, o_busy_nxt, to_module_valid, to_module_valid_nxt;
   wire [127:0] crc_final_out, minmax_final_out;
   wire crc_o_valid, minmax_o_valid, crypt_o_valid;
+  wire crypt_enable, crc_enable, minmax_enable;
 
   ENCRYPT crypt0 (
     .i_clk(clk),
     .i_rst(rst),
     .plaintext(data[63:0]),
     .key(divisor[63:0]),
-    .text_intermediate(data[111:64]),
     .i_valid(to_module_valid),
+    .i_enable(crypt_enable),
     .i_encrypt(fn_sel == FN_ENCRYPT),
     .o_plaintext(crypt_plaintext_out),
     .o_key(crypt_key_out),
-    .o_text_intermediate(crypt_text_intermediate_out),
     .o_valid(crypt_o_valid)
   );
 
   CRC crc0 (
     .i_clk(clk),
     .i_rst(rst),
-    .i_valid(to_module_valid_nxt || in_en),
+    .i_valid(to_module_valid_nxt),
     .i_remainder(data[2:0]),
     .iot_in(loaded_data[7:0]),
+    .i_enable(crc_enable),
     // .i_divisor(divisor),
     .o_remainder(crc_remainder_out[2:0]),
     // .o_divisor(crc_divisor_out),
@@ -75,6 +76,7 @@ output [127:0] iot_out;
     .iot_data(loaded_data),
     .i_valid(to_module_valid_nxt),
     .i_Max(fn_sel == FN_TOP2MAX),
+    .i_enable(minmax_enable),
     .reg1(data[127:0]),
     .reg2(divisor[127:0]),
     .o_reg1(minmax_reg1_out),
@@ -86,6 +88,10 @@ output [127:0] iot_out;
   assign busy = o_busy_nxt; //! be careful, not reg
   assign valid = total_o_valid;
   assign iot_out = iot_data_out_nxt;
+
+  assign crypt_enable = (fn_sel == FN_ENCRYPT || fn_sel == FN_DECRYPT);
+  assign crc_enable = (fn_sel == FN_CRC_GEN);
+  assign minmax_enable = (fn_sel == FN_TOP2MAX || fn_sel == FN_LAST2MIN);
 
 
 
@@ -101,11 +107,6 @@ output [127:0] iot_out;
           FN_TOP2MAX, FN_LAST2MIN: state_nxt = S_MINMAX;
         endcase
       end
-      // S_MINMAX: begin
-      //   if(cnt_load == 15 && cnt_data == 3'd6)
-      //     state_nxt = S_OUTPUT;
-      // end
-      // S_OUTPUT: state_nxt = S_MINMAX;
     endcase
   end
 
@@ -119,13 +120,13 @@ output [127:0] iot_out;
         cnt_data_nxt = 0;
         o_busy_nxt = 0;
       end
-      S_LOAD: begin
-        if(in_en)
-          cnt_load_nxt = cnt_load + 1;
-        if(cnt_load == 15) begin
-          cnt_data_nxt = cnt_data + 1;
-        end
-      end
+      // S_LOAD: begin
+      //   if(in_en)
+      //     cnt_load_nxt = cnt_load + 1;
+      //   if(cnt_load == 15) begin
+      //     cnt_data_nxt = cnt_data + 1;
+      //   end
+      // end
       S_MINMAX, S_CRC: begin
         o_busy_nxt = 0;
         if(in_en)
@@ -155,23 +156,23 @@ output [127:0] iot_out;
     total_o_valid = 0;
     iot_data_out_nxt = 0;
     to_module_valid_nxt = 0;
-    case(fn_sel)
-      FN_DECRYPT, FN_ENCRYPT: begin
+    case(state)
+      S_CRYPT: begin
         total_o_valid = crypt_o_valid;
         iot_data_out_nxt = {divisor[127:64], crypt_plaintext_out};
         if(crypt_o_valid || (cnt_load == 15 && cnt_data == 0))
           to_module_valid_nxt = 1;
         // to_module_valid = (cnt_load == 0 && cnt_data[2:0] != 0);
       end
-      FN_CRC_GEN: begin
+      S_CRC: begin
         total_o_valid = crc_o_valid;
         iot_data_out_nxt = {{124'b0}, crc_remainder_out[6:3]};
-        if(cnt_load != 0 && cnt_data != 0)
+        if(!(cnt_load == 0 && cnt_data == 0 && cnt_load_nxt != 1))
           to_module_valid_nxt = 1;
       
         // to_module_valid_nxt = (cnt_load == 0 && cnt_data != 0 && in_en);
       end
-      FN_TOP2MAX, FN_LAST2MIN: begin
+      S_MINMAX: begin
         total_o_valid = minmax_o_valid;
         iot_data_out_nxt = minmax_final_out;
         to_module_valid_nxt = (cnt_load == 0 && cnt_data != 0);
@@ -198,11 +199,11 @@ output [127:0] iot_out;
           // divisor_nxt = {{3'b0}, divisor[127:64], crypt_key_out};
         end
         if(crypt_o_valid || (cnt_load == 15 && cnt_data == 0)) begin
-          data_nxt = {{67'b0}, loaded_data_nxt[63:0]};    // plaintext
-          divisor_nxt = {{3'b0}, loaded_data_nxt[127:64], loaded_data_nxt[127:64]};   // key
+          data_nxt = {{64'b0}, loaded_data_nxt[63:0]};    // plaintext
+          divisor_nxt = {loaded_data_nxt[127:64], loaded_data_nxt[127:64]};   // key
         end else begin
-          data_nxt = {{19'b0}, crypt_text_intermediate_out, crypt_plaintext_out};
-          divisor_nxt = {{3'b0}, divisor[127:64], crypt_key_out};
+          data_nxt = {{16'b0}, {48'b0}, crypt_plaintext_out};
+          divisor_nxt = {divisor[127:64], crypt_key_out};
         end
         
       end
@@ -210,7 +211,7 @@ output [127:0] iot_out;
         if(in_en) begin
           loaded_data_nxt[7:0] = iot_in;
         end
-        data_nxt = {{123'b0}, crc_remainder_out};
+        data_nxt = {{121'b0}, crc_remainder_out};
       end
     endcase
   end
@@ -247,12 +248,11 @@ module ENCRYPT(
   input i_rst,
   input [63:0] plaintext,
   input [63:0] key,
-  input [47:0] text_intermediate,
   input i_valid,
   input i_encrypt,    // 1: encrypt, 0: decrypt
+  input i_enable,
   output [63:0] o_plaintext,
   output [63:0] o_key,
-  output [47:0] o_text_intermediate,
   output o_valid
 );
   // state
@@ -263,54 +263,56 @@ module ENCRYPT(
   localparam S_FIN_PERMUTATION = 3'd4;
   localparam S_DONE = 3'd5;
   // PC2 lookup table
-  reg [5:0] PC1 [55:0] = '{6'd7,6'd15,6'd23,6'd31,6'd39,6'd47,6'd55,6'd63,6'd6,6'd14,6'd22,6'd30,6'd38,6'd46,6'd54,6'd62,6'd5,6'd13,6'd21,6'd29,6'd37,6'd45,6'd53,6'd61,6'd4,6'd12,6'd20,6'd28,6'd1,6'd9,6'd17,6'd25,6'd33,6'd41,6'd49,6'd57,6'd2,6'd10,6'd18,6'd26,6'd34,6'd42,6'd50,6'd58,6'd3,6'd11,6'd19,6'd27,6'd35,6'd43,6'd51,6'd59,6'd36,6'd44,6'd52,6'd60};
-  reg [5:0] PC2 [47:0] = '{6'd42, 6'd39, 6'd45, 6'd32, 6'd55, 6'd51, 6'd53, 6'd28, 6'd41, 6'd50, 6'd35, 6'd46, 6'd33, 6'd37, 6'd44, 6'd52, 6'd30, 6'd48, 6'd40, 6'd49, 6'd29, 6'd36, 6'd43, 6'd54, 6'd15, 6'd4, 6'd25, 6'd19, 6'd9, 6'd1, 6'd26, 6'd16, 6'd5, 6'd11, 6'd23, 6'd8, 6'd12, 6'd7, 6'd17, 6'd0, 6'd22, 6'd3, 6'd10, 6'd14, 6'd6, 6'd20, 6'd27, 6'd24};
-  reg [3:0] S1 [0:3][0:15] = '{'{4'd14, 4'd4, 4'd13, 4'd1, 4'd2, 4'd15, 4'd11, 4'd8, 4'd3, 4'd10, 4'd6, 4'd12, 4'd5, 4'd9, 4'd0, 4'd7},
+  localparam [5:0] PC1 [55:0] = '{6'd7,6'd15,6'd23,6'd31,6'd39,6'd47,6'd55,6'd63,6'd6,6'd14,6'd22,6'd30,6'd38,6'd46,6'd54,6'd62,6'd5,6'd13,6'd21,6'd29,6'd37,6'd45,6'd53,6'd61,6'd4,6'd12,6'd20,6'd28,6'd1,6'd9,6'd17,6'd25,6'd33,6'd41,6'd49,6'd57,6'd2,6'd10,6'd18,6'd26,6'd34,6'd42,6'd50,6'd58,6'd3,6'd11,6'd19,6'd27,6'd35,6'd43,6'd51,6'd59,6'd36,6'd44,6'd52,6'd60};
+  localparam [5:0] PC2 [47:0] = '{6'd42, 6'd39, 6'd45, 6'd32, 6'd55, 6'd51, 6'd53, 6'd28, 6'd41, 6'd50, 6'd35, 6'd46, 6'd33, 6'd37, 6'd44, 6'd52, 6'd30, 6'd48, 6'd40, 6'd49, 6'd29, 6'd36, 6'd43, 6'd54, 6'd15, 6'd4, 6'd25, 6'd19, 6'd9, 6'd1, 6'd26, 6'd16, 6'd5, 6'd11, 6'd23, 6'd8, 6'd12, 6'd7, 6'd17, 6'd0, 6'd22, 6'd3, 6'd10, 6'd14, 6'd6, 6'd20, 6'd27, 6'd24};
+  localparam [3:0] S1 [0:3][0:15] = '{'{4'd14, 4'd4, 4'd13, 4'd1, 4'd2, 4'd15, 4'd11, 4'd8, 4'd3, 4'd10, 4'd6, 4'd12, 4'd5, 4'd9, 4'd0, 4'd7},
   '{4'd0, 4'd15, 4'd7, 4'd4, 4'd14, 4'd2, 4'd13, 4'd1, 4'd10, 4'd6, 4'd12, 4'd11, 4'd9, 4'd5, 4'd3, 4'd8},
   '{4'd4, 4'd1, 4'd14, 4'd8, 4'd13, 4'd6, 4'd2, 4'd11, 4'd15, 4'd12, 4'd9, 4'd7, 4'd3, 4'd10, 4'd5, 4'd0},
   '{4'd15, 4'd12, 4'd8, 4'd2, 4'd4, 4'd9, 4'd1, 4'd7, 4'd5, 4'd11, 4'd3, 4'd14, 4'd10, 4'd0, 4'd6, 4'd13}
   };
-  reg [3:0] S2 [0:3][0:15] = '{'{4'd15, 4'd1, 4'd8, 4'd14, 4'd6, 4'd11, 4'd3, 4'd4, 4'd9, 4'd7, 4'd2, 4'd13, 4'd12, 4'd0, 4'd5, 4'd10},
+  localparam [3:0] S2 [0:3][0:15] = '{'{4'd15, 4'd1, 4'd8, 4'd14, 4'd6, 4'd11, 4'd3, 4'd4, 4'd9, 4'd7, 4'd2, 4'd13, 4'd12, 4'd0, 4'd5, 4'd10},
   '{4'd3, 4'd13, 4'd4, 4'd7, 4'd15, 4'd2, 4'd8, 4'd14, 4'd12, 4'd0, 4'd1, 4'd10, 4'd6, 4'd9, 4'd11, 4'd5},
   '{4'd0, 4'd14, 4'd7, 4'd11, 4'd10, 4'd4, 4'd13, 4'd1, 4'd5, 4'd8, 4'd12, 4'd6, 4'd9, 4'd3, 4'd2, 4'd15},
   '{4'd13, 4'd8, 4'd10, 4'd1, 4'd3, 4'd15, 4'd4, 4'd2, 4'd11, 4'd6, 4'd7, 4'd12, 4'd0, 4'd5, 4'd14, 4'd9}
   };
-  reg [3:0] S3 [0:3][0:15] = '{'{4'd10, 4'd0, 4'd9, 4'd14, 4'd6, 4'd3, 4'd15, 4'd5, 4'd1, 4'd13, 4'd12, 4'd7, 4'd11, 4'd4, 4'd2, 4'd8},
+  localparam [3:0] S3 [0:3][0:15] = '{'{4'd10, 4'd0, 4'd9, 4'd14, 4'd6, 4'd3, 4'd15, 4'd5, 4'd1, 4'd13, 4'd12, 4'd7, 4'd11, 4'd4, 4'd2, 4'd8},
   '{4'd13, 4'd7, 4'd0, 4'd9, 4'd3, 4'd4, 4'd6, 4'd10, 4'd2, 4'd8, 4'd5, 4'd14, 4'd12, 4'd11, 4'd15, 4'd1},
   '{4'd13, 4'd6, 4'd4, 4'd9, 4'd8, 4'd15, 4'd3, 4'd0, 4'd11, 4'd1, 4'd2, 4'd12, 4'd5, 4'd10, 4'd14, 4'd7},
   '{4'd1, 4'd10, 4'd13, 4'd0, 4'd6, 4'd9, 4'd8, 4'd7, 4'd4, 4'd15, 4'd14, 4'd3, 4'd11, 4'd5, 4'd2, 4'd12}
   };
-  reg [3:0] S4 [0:3][0:15] = '{'{4'd7, 4'd13, 4'd14, 4'd3, 4'd0, 4'd6, 4'd9, 4'd10, 4'd1, 4'd2, 4'd8, 4'd5, 4'd11, 4'd12, 4'd4, 4'd15},
+  localparam [3:0] S4 [0:3][0:15] = '{'{4'd7, 4'd13, 4'd14, 4'd3, 4'd0, 4'd6, 4'd9, 4'd10, 4'd1, 4'd2, 4'd8, 4'd5, 4'd11, 4'd12, 4'd4, 4'd15},
   '{4'd13, 4'd8, 4'd11, 4'd5, 4'd6, 4'd15, 4'd0, 4'd3, 4'd4, 4'd7, 4'd2, 4'd12, 4'd1, 4'd10, 4'd14, 4'd9},
   '{4'd10, 4'd6, 4'd9, 4'd0, 4'd12, 4'd11, 4'd7, 4'd13, 4'd15, 4'd1, 4'd3, 4'd14, 4'd5, 4'd2, 4'd8, 4'd4},
   '{4'd3, 4'd15, 4'd0, 4'd6, 4'd10, 4'd1, 4'd13, 4'd8, 4'd9, 4'd4, 4'd5, 4'd11, 4'd12, 4'd7, 4'd2, 4'd14}
   };
-  reg [3:0] S5 [0:3][0:15] = '{'{4'd2, 4'd12, 4'd4, 4'd1, 4'd7, 4'd10, 4'd11, 4'd6, 4'd8, 4'd5, 4'd3, 4'd15, 4'd13, 4'd0, 4'd14, 4'd9},
+  localparam [3:0] S5 [0:3][0:15] = '{'{4'd2, 4'd12, 4'd4, 4'd1, 4'd7, 4'd10, 4'd11, 4'd6, 4'd8, 4'd5, 4'd3, 4'd15, 4'd13, 4'd0, 4'd14, 4'd9},
   '{4'd14, 4'd11, 4'd2, 4'd12, 4'd4, 4'd7, 4'd13, 4'd1, 4'd5, 4'd0, 4'd15, 4'd10, 4'd3, 4'd9, 4'd8, 4'd6},
   '{4'd4, 4'd2, 4'd1, 4'd11, 4'd10, 4'd13, 4'd7, 4'd8, 4'd15, 4'd9, 4'd12, 4'd5, 4'd6, 4'd3, 4'd0, 4'd14},
   '{4'd11, 4'd8, 4'd12, 4'd7, 4'd1, 4'd14, 4'd2, 4'd13, 4'd6, 4'd15, 4'd0, 4'd9, 4'd10, 4'd4, 4'd5, 4'd3}
   };
-  reg [3:0] S6 [0:3][0:15] = '{'{4'd12, 4'd1, 4'd10, 4'd15, 4'd9, 4'd2, 4'd6, 4'd8, 4'd0, 4'd13, 4'd3, 4'd4, 4'd14, 4'd7, 4'd5, 4'd11},
+  localparam [3:0] S6 [0:3][0:15] = '{'{4'd12, 4'd1, 4'd10, 4'd15, 4'd9, 4'd2, 4'd6, 4'd8, 4'd0, 4'd13, 4'd3, 4'd4, 4'd14, 4'd7, 4'd5, 4'd11},
   '{4'd10, 4'd15, 4'd4, 4'd2, 4'd7, 4'd12, 4'd9, 4'd5, 4'd6, 4'd1, 4'd13, 4'd14, 4'd0, 4'd11, 4'd3, 4'd8},
   '{4'd9, 4'd14, 4'd15, 4'd5, 4'd2, 4'd8, 4'd12, 4'd3, 4'd7, 4'd0, 4'd4, 4'd10, 4'd1, 4'd13, 4'd11, 4'd6},
   '{4'd4, 4'd3, 4'd2, 4'd12, 4'd9, 4'd5, 4'd15, 4'd10, 4'd11, 4'd14, 4'd1, 4'd7, 4'd6, 4'd0, 4'd8, 4'd13}
   };
-  reg [3:0] S7 [0:3][0:15] = '{'{4'd4, 4'd11, 4'd2, 4'd14, 4'd15, 4'd0, 4'd8, 4'd13, 4'd3, 4'd12, 4'd9, 4'd7, 4'd5, 4'd10, 4'd6, 4'd1},
+  localparam [3:0] S7 [0:3][0:15] = '{'{4'd4, 4'd11, 4'd2, 4'd14, 4'd15, 4'd0, 4'd8, 4'd13, 4'd3, 4'd12, 4'd9, 4'd7, 4'd5, 4'd10, 4'd6, 4'd1},
   '{4'd13, 4'd0, 4'd11, 4'd7, 4'd4, 4'd9, 4'd1, 4'd10, 4'd14, 4'd3, 4'd5, 4'd12, 4'd2, 4'd15, 4'd8, 4'd6},
   '{4'd1, 4'd4, 4'd11, 4'd13, 4'd12, 4'd3, 4'd7, 4'd14, 4'd10, 4'd15, 4'd6, 4'd8, 4'd0, 4'd5, 4'd9, 4'd2},
   '{4'd6, 4'd11, 4'd13, 4'd8, 4'd1, 4'd4, 4'd10, 4'd7, 4'd9, 4'd5, 4'd0, 4'd15, 4'd14, 4'd2, 4'd3, 4'd12}
   };
-  reg [3:0] S8 [0:3][0:15] = '{'{4'd13, 4'd2, 4'd8, 4'd4, 4'd6, 4'd15, 4'd11, 4'd1, 4'd10, 4'd9, 4'd3, 4'd14, 4'd5, 4'd0, 4'd12, 4'd7},
+
+  localparam [3:0] S8 [0:3][0:15] = '{'{4'd13, 4'd2, 4'd8, 4'd4, 4'd6, 4'd15, 4'd11, 4'd1, 4'd10, 4'd9, 4'd3, 4'd14, 4'd5, 4'd0, 4'd12, 4'd7},
   '{4'd1, 4'd15, 4'd13, 4'd8, 4'd10, 4'd3, 4'd7, 4'd4, 4'd12, 4'd5, 4'd6, 4'd11, 4'd0, 4'd14, 4'd9, 4'd2},
   '{4'd7, 4'd11, 4'd4, 4'd1, 4'd9, 4'd12, 4'd14, 4'd2, 4'd0, 4'd6, 4'd10, 4'd13, 4'd15, 4'd3, 4'd5, 4'd8},
   '{4'd2, 4'd1, 4'd14, 4'd7, 4'd4, 4'd10, 4'd8, 4'd13, 4'd15, 4'd12, 4'd9, 4'd0, 4'd3, 4'd5, 4'd6, 4'd11}
   };
-  reg [4:0] P [31:0] =  '{5'd16,5'd25,5'd12,5'd11,5'd3,5'd20,5'd4,5'd15,5'd31,5'd17,5'd9,5'd6,5'd27,5'd14,5'd1,5'd22,5'd30,5'd24,5'd8,5'd18,5'd0,5'd5,5'd29,5'd23,5'd13,5'd19,5'd2,5'd26,5'd10,5'd21,5'd28,5'd7};
-  reg [5:0] Expansion [47:0] = '{6'd0,6'd31,6'd30,6'd29,6'd28,6'd27,6'd28,6'd27,6'd26,6'd25,6'd24,6'd23,6'd24,6'd23,6'd22,6'd21,6'd20,6'd19,6'd20,6'd19,6'd18,6'd17,6'd16,6'd15,6'd16,6'd15,6'd14,6'd13,6'd12,6'd11,6'd12,6'd11,6'd10,6'd9,6'd8,6'd7,6'd8,6'd7,6'd6,6'd5,6'd4,6'd3,6'd4,6'd3,6'd2,6'd1,6'd0,6'd31};
-  reg [5:0] Final_permutation [63:0] = '{6'd24,6'd56,6'd16,6'd48,6'd8,6'd40,6'd0,6'd32,6'd25,6'd57,6'd17,6'd49,6'd9,6'd41,6'd1,6'd33,6'd26,6'd58,6'd18,6'd50,6'd10,6'd42,6'd2,6'd34,6'd27,6'd59,6'd19,6'd51,6'd11,6'd43,6'd3,6'd35,6'd28,6'd60,6'd20,6'd52,6'd12,6'd44,6'd4,6'd36,6'd29,6'd61,6'd21,6'd53,6'd13,6'd45,6'd5,6'd37,6'd30,6'd62,6'd22,6'd54,6'd14,6'd46,6'd6,6'd38,6'd31,6'd63,6'd23,6'd55,6'd15,6'd47,6'd7,6'd39};
+  localparam [4:0] P [31:0] =  '{5'd16,5'd25,5'd12,5'd11,5'd3,5'd20,5'd4,5'd15,5'd31,5'd17,5'd9,5'd6,5'd27,5'd14,5'd1,5'd22,5'd30,5'd24,5'd8,5'd18,5'd0,5'd5,5'd29,5'd23,5'd13,5'd19,5'd2,5'd26,5'd10,5'd21,5'd28,5'd7};
+  localparam [5:0] Expansion [47:0] = '{6'd0,6'd31,6'd30,6'd29,6'd28,6'd27,6'd28,6'd27,6'd26,6'd25,6'd24,6'd23,6'd24,6'd23,6'd22,6'd21,6'd20,6'd19,6'd20,6'd19,6'd18,6'd17,6'd16,6'd15,6'd16,6'd15,6'd14,6'd13,6'd12,6'd11,6'd12,6'd11,6'd10,6'd9,6'd8,6'd7,6'd8,6'd7,6'd6,6'd5,6'd4,6'd3,6'd4,6'd3,6'd2,6'd1,6'd0,6'd31};
+  localparam [5:0] Final_permutation [63:0] = '{6'd24,6'd56,6'd16,6'd48,6'd8,6'd40,6'd0,6'd32,6'd25,6'd57,6'd17,6'd49,6'd9,6'd41,6'd1,6'd33,6'd26,6'd58,6'd18,6'd50,6'd10,6'd42,6'd2,6'd34,6'd27,6'd59,6'd19,6'd51,6'd11,6'd43,6'd3,6'd35,6'd28,6'd60,6'd20,6'd52,6'd12,6'd44,6'd4,6'd36,6'd29,6'd61,6'd21,6'd53,6'd13,6'd45,6'd5,6'd37,6'd30,6'd62,6'd22,6'd54,6'd14,6'd46,6'd6,6'd38,6'd31,6'd63,6'd23,6'd55,6'd15,6'd47,6'd7,6'd39};
   reg [3:0] cnt, cnt_nxt;
   reg [2:0] state, state_nxt;
-  reg [63:0] plaintext_nxt, key_nxt, key_tmp, key_permuted, text_expended;
+  reg [63:0] plaintext_nxt, key_nxt, key_tmp, key_permuted;
+  reg [47:0] text_expended;
   reg [47:0] key_now, text_tmp;
   wire [31:0] text_intermediate_nxt;
   reg [31:0]  F_out, R_nxt, L_nxt;
@@ -321,10 +323,10 @@ module ENCRYPT(
   integer i;
 
   assign shift_one_bit = (cnt == 6 || cnt == 13 || state == S_GEN_K1);
-  assign o_text_intermediate = text_intermediate_nxt;
+  // assign o_text_intermediate = text_intermediate_nxt;
   assign o_key = key_nxt;
   assign o_plaintext = plaintext_nxt;
-  assign o_valid = (state == S_FIN_PERMUTATION);
+  assign o_valid = (state == S_DONE);
   assign R_now = plaintext[31:0];
   assign L_now = plaintext[63:32];
 
@@ -351,7 +353,7 @@ module ENCRYPT(
       S_ENCRYPT: begin
         if(cnt == 14) state_nxt = S_FIN_PERMUTATION;
       end
-      S_FIN_PERMUTATION: state_nxt = S_PC1_ROTATE; //! need modification
+      S_FIN_PERMUTATION: state_nxt = S_DONE; //! need modification
       S_DONE: state_nxt = S_IDLE; 
     endcase
   end
@@ -375,16 +377,6 @@ module ENCRYPT(
     key_permuted = 0;
     case(state)
       S_PC1_ROTATE: begin
-        // for(i=0; i<8; i=i+1) begin    // PC1
-        //   key_permuted[55-i] = key[7+(i<<3)];
-        //   key_permuted[47-i] = key[6+(i<<3)];
-        //   key_permuted[39-i] = key[5+(i<<3)];
-        //   key_permuted[31-i] = key[4+(i<<3)];
-        //   key_permuted[23-i] = key[1+(i<<3)];
-        //   key_permuted[15-i] = key[2+(i<<3)];
-        //   key_permuted[7-i] = key[3+(i<<3)];
-        // end
-        // key_tmp = {key_permuted[55:28], key_permuted[23:0], key_permuted[27:24]};    // for completing PC1 (left rotate by 4)
         for(i=0; i<56; i=i+1) begin
           key_tmp[i] = key[PC1[i]];
         end
@@ -467,6 +459,9 @@ module ENCRYPT(
           plaintext_nxt[i] = plaintext[Final_permutation[i]];
         end
       end
+      S_DONE: begin
+        plaintext_nxt = plaintext;
+      end
     endcase
     
   end
@@ -476,7 +471,7 @@ module ENCRYPT(
     if(i_rst) begin
       state <= S_IDLE;
       cnt <= 0;
-    end else begin
+    end else if(i_enable) begin
       state <= state_nxt;
       cnt <= cnt_nxt;
     end
@@ -490,6 +485,7 @@ module CRC(
   input i_valid,
   input [2:0] i_remainder,
   input [7:0] iot_in,
+  input i_enable,
 
   // input [130:0] i_divisor,
   output [2:0] o_remainder,
@@ -580,6 +576,9 @@ module CRC(
   always @(*) begin
     o_remainder_nxt = i_remainder;
     case(state)
+      // S_IDLE: begin
+      //   if(i_valid) o_remainder_nxt = calcA(iot_in, {3'b000});
+      // end
       S_CALC_A: begin
         if(cnt == 0) 
           o_remainder_nxt = calcA(iot_in, {3'b000});
@@ -598,7 +597,7 @@ module CRC(
       state <= S_IDLE;
       cnt <= 0;
       o_valid_reg <= 0;
-    end else begin
+    end else if(i_enable) begin
       state <= state_nxt;
       cnt <= cnt_nxt;
       o_valid_reg <= o_valid_nxt;
@@ -612,6 +611,7 @@ module MinMax(
   input [127:0] iot_data,
   input i_valid,
   input i_Max,   // 1: top2max, 0: last2min
+  input i_enable,
   input [127:0] reg1,
   input [127:0] reg2,
   output [127:0] o_reg1,
@@ -699,7 +699,7 @@ module MinMax(
     if(i_rst) begin
       state <= S_IDLE;
       cnt <= 0;
-    end else begin
+    end else if(i_enable) begin
       state <= state_nxt;
       cnt <= cnt_nxt;
     end
